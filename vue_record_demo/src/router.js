02d2e151a498dd55aa2555f83a2557edd03eb05d
Tycalwayslove <1186446929@qq.com>
2019-04-04 16:01:06 +0800

    暂定一级的路由

--- vue_record_demo/src/router.js
+++ vue_record_demo/src/router.js
@@ -1,36 +1,46 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Index from "./views/index.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Index from './views/index.vue';
 
-Vue.use(Router);
+Vue.use(Router)
 const NumberGrowWrap = resolve =>
-  require(["@/page/demo/NumberGrowWrap/NumberGrowWrap"], resolve);
+  require(['@/page/demo/NumberGrowWrap/NumberGrowWrap'], resolve)
+const Collapse = resolve =>
+  require(['@/page/ComponentDemo/collapse/demo'], resolve)
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "index",
+      path: '/',
+      name: 'index',
       component: Index
     },
     {
-      path: "/demo",
-      name: "Demo",
+      path: '/demo',
+      name: 'Demo',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./page/demo/index.vue")
+        import(/* webpackChunkName: "about" */ './page/demo/index.vue')
     },
     {
-      path: "/numbergrowwrap",
-      name: "NumberGrowWrap",
+      path: '/numbergrowwrap',
+      name: 'NumberGrowWrap',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./page/demo/NumberGrowWrap/demo.vue")
+        import(/* webpackChunkName: "about" */ './page/demo/NumberGrowWrap/demo.vue')
+    },
+    {
+      path: '/collapse',
+      name: 'Collapse',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: Collapse
     }
   ]
-});
+})
